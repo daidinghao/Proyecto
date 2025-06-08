2c62ae1115e6e4d081a4e94fd5083fa59c7b2d0d
daidinghao <daidinghaosme@gmail.com>
2025-06-08 08:49:40 +0200

    update db

--- db.py
+++ db.py
@@ -1,11 +1,24 @@
 import os
 import psycopg2
+import urllib.parse as urlparse
 
 def get_db_connection():
-    return psycopg2.connect(
-        host=os.environ.get("DB_HOST"),
-        port=os.environ.get("DB_PORT", "5432"),
-        dbname=os.environ.get("DB_NAME"),
-        user=os.environ.get("DB_USER"),
-        password=os.environ.get("DB_PASSWORD")
-    )
+    db_url = os.environ.get("DATABASE_URL")
+    if db_url:
+        result = urlparse.urlparse(db_url)
+        return psycopg2.connect(
+            dbname=result.path[1:],
+            user=result.username,
+            password=result.password,
+            host=result.hostname,
+            port=result.port
+        )
+    else:
+        # respaldo para desarrollo local o configuración manual de ENV
+        return psycopg2.connect(
+            host=os.environ.get("DB_HOST", "localhost"),
+            port=os.environ.get("DB_PORT", "5432"),
+            dbname=os.environ.get("DB_NAME"),
+            user=os.environ.get("DB_USER"),
+            password=os.environ.get("DB_PASSWORD")
+        )
