fbbddd4e41244ba56ea0269e4d8bcf5f70acbb2e
daidinghao <daidinghaosme@gmail.com>
2025-06-08 09:14:52 +0200

    update db

--- db.py
+++ db.py
@@ -4,21 +4,14 @@ import urllib.parse as urlparse
 
 def get_db_connection():
     db_url = os.environ.get("DATABASE_URL")
-    if db_url:
-        result = urlparse.urlparse(db_url)
-        return psycopg2.connect(
-            dbname=result.path[1:],
-            user=result.username,
-            password=result.password,
-            host=result.hostname,
-            port=result.port
-        )
-    else:
-        # respaldo para desarrollo local o configuración manual de ENV
-        return psycopg2.connect(
-            host=os.environ.get("DB_HOST", "localhost"),
-            port=os.environ.get("DB_PORT", "5432"),
-            dbname=os.environ.get("DB_NAME"),
-            user=os.environ.get("DB_USER"),
-            password=os.environ.get("DB_PASSWORD")
-        )
+    if not db_url:
+        raise Exception("Variable de entorno DATABASE_URL no establecida")
+
+    result = urlparse.urlparse(db_url)
+    return psycopg2.connect(
+        dbname=result.path[1:],
+        user=result.username,
+        password=result.password,
+        host=result.hostname,
+        port=result.port
+    )
